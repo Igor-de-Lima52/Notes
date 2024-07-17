3cb1129da3828050e34ebd7592c80f6941597ff7
Igor <delimaigor781@gmail.com>
2024-07-17 18:37:53 -0300

    Authentication has been done

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes.js");
 const notesRouter = require("./notes.routes.js");
 const tagsRouter = require("./tags.routes.js");
+const sessionRouter = require("./sessions.routes.js");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
 
